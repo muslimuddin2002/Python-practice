ae4f702ab74887a8aa522a3219b75e5eec1c513d
MUSLIM UDDIN <144557545+muslimuddin2002@users.noreply.github.com>
2026-08-05 09:59:06 +0600

    Add polynomial evaluator for y = 3x^3 - 2x^2 + 3x - 1

---
+++ polynomial_evaluator.py
@@ -0,0 +1,7 @@
+# Problem: Evaluate the polynomial y = 3x^3 - 2x^2 + 3x - 1
+# for a given hardcoded value of x.
+
+x = 0  # Hardcode your test data here.
+x = float(x)
+y = 3 * x**3 - 2 * x**2 + 3 * x - 1
+print("y =", y)
