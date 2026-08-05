58b2c6e54d4028dc93b07f4e24ffe73af0cf6e9f
MUSLIM UDDIN <144557545+muslimuddin2002@users.noreply.github.com>
2026-08-05 10:25:35 +0600

    Enhance comments for clarity in seconds computation
    Added comments to improve code readability.

---
+++ comments_and_readability.py
@@ -0,0 +1,7 @@
+# This program computes the number of seconds in a given number of hours.
+
+hours = 2  # number of hours
+seconds_in_hour = 3600  # number of seconds in 1 hour
+
+print("Hours: ", hours)  # printing the number of hours
+print("Seconds in Hours: ", hours * seconds_in_hour)  # printing the number of seconds in a given number of hours
