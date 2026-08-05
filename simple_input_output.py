48d31ad6de194c04ca93386217a25ad25a2c8b7a
MUSLIM UDDIN <144557545+muslimuddin2002@users.noreply.github.com>
2026-08-05 11:08:30 +0600

    Implement basic arithmetic operations with user input
    This script performs basic arithmetic operations on two user-provided float values.

---
+++ simple_input_output.py
@@ -0,0 +1,13 @@
+# Task: Evaluate the results of four basic arithmetic operations
+# (addition, subtraction, multiplication, division) using two
+# float values entered by the user.
+
+a = float(input("Enter value for a: "))
+b = float(input("Enter value for b: "))
+
+print("a + b =", a + b)
+print("a - b =", a - b)
+print("a * b =", a * b)
+print("a / b =", a / b)
+
+print("\nThat's all, folks!")
