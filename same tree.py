bfb0d33dbe538ee8ae0bcf208d87afd10cda9a90
MUSLIM UDDIN <144557545+muslimuddin2002@users.noreply.github.com>
2026-08-07 16:32:22 +0600

    Add isSameTree method to Solution class
    Implement isSameTree method to compare two binary trees.

---
+++ same tree.py
@@ -0,0 +1,14 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if p is None and q is None:
+            return True
+        if q and p and p.val == q.val:
+            return self. isSameTree(q.left,p.left) and self. isSameTree(q.right,p.right) 
+        return False       
+        
